56aa4c0dcc5594fe7e981f07330dab7c3fcda59f
yuntai <yuntai.kyong@gmail.com>
2021-06-05 19:26:22 +0900

    update

--- fit_all.sh
+++ fit_all.sh
@@ -1,4 +1,11 @@
-SEED=42
-for i in $(seq $1 $2); do
-    python fit_xgb.py --num $i --seed $SEED
-done
+#!/bin/bash
+if [ -f /.dockerenv ]; then
+    docker exec -it colab $0 $*
+else
+    set -euo pipefail
+    SEED=42
+    for i in $(seq $1 $2); do
+        python fit_xgb.py --num $i --seed $SEED
+    done
+fi
+
--- run_docker.sh
+++ run_docker.sh
@@ -14,4 +14,5 @@ echo "dataroot($dataroot)"
 docker stop tacotron2 || true && docker rm tacotron2 || true
 docker stop colab || true && docker rm colab || true
 
-docker run --gpus all --shm-size=1g --rm --name colab --ulimit memlock=-1 --ulimit stack=67108864 -d --ipc=host --ip 0.0.0.0 -p 9999:8888 -v /mnt/tmp:/mnt/tmp -v $(pwd):/workspace -v ${dataroot}:${dataroot} colab
+#docker run --gpus all --shm-size=1g --rm --name colab --ulimit memlock=-1 --ulimit stack=67108864 -d --ipc=host --ip 0.0.0.0 -p 9999:8888 -v /mnt/tmp:/mnt/tmp -v $(pwd):/workspace -v ${dataroot}:${dataroot} colab
+docker run --shm-size=1g --rm --name colab --ulimit memlock=-1 --ulimit stack=67108864 -d --ipc=host --ip 0.0.0.0 -p 9999:8888 -v /mnt/tmp:/mnt/tmp -v /mnt/datasets:/mnt/datasets -v $(pwd):/workspace -v ${dataroot}:${dataroot} colab
