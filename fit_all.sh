41463bda4d84c4b2b80938f20298a3a7ac5d63d1
yuntai <yuntai.kyong@gmail.com>
2021-06-05 19:29:20 +0900

    fix

--- fit_all.sh
+++ fit_all.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 if [ -f /.dockerenv ]; then
-    docker exec -it colab $0 $*
-else
     set -euo pipefail
     SEED=42
     for i in $(seq $1 $2); do
         python fit_xgb.py --num $i --seed $SEED
     done
+else
+    docker exec -it colab bash $0 $*
 fi
 
